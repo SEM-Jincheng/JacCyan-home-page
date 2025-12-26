6181ba01d408e7c555eda164fb229ed62e30e4c6
Jac Cyan <609652265@qq.com>
2025-12-27 01:47:10 +0800

    修改了图标路径

--- src/config.js
+++ src/config.js
@@ -148,7 +148,7 @@ const config = {
 		{ go: "🔍 前往", img: "/img/sunshine.jpg", title: "Project 8", subtitle: "8,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://leleo.top", show: false },
 	],
 
-	statement: ["备案号：XXICP备123456789号", "Copyright © 2025 Leleo"],
+	statement: ["备案号：豫ICP备2025159834号", "Copyright © 2025 Leleo"],
 }
 
 export default config
